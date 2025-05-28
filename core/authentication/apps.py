2aa5f6af033182b97009f25da111679b331b443f
Williams392 <williamsvalle392@gmail.com>
2025-05-28 18:48:20 -0500

    chore: update database mysql

--- core/authentication/apps.py
+++ core/authentication/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comprobante_pago'
+    name = 'authentication'
